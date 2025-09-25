89f65c9ada4a0c126b5e2c56026e6d5de9ad6156
tuan nelet <tuannelet@gmail.com>
2025-09-25 21:56:42 +0700

    testing test file

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "✅ PHP fonctionne !<br>";
+echo "DB_HOST = " . getenv("DB_HOST") . "<br>";
+echo "DB_USER = " . getenv("DB_USER") . "<br>";
+echo "DB_NAME = " . getenv("DB_NAME") . "<br>";
