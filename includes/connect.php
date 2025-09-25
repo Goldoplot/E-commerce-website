054c0b2f70184cbf670b2ffbb07bd13c45b52213
tuan nelet <tuannelet@gmail.com>
2025-09-26 02:32:42 +0700

    caddyfile and connect modified

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,10 @@
 <?php
 
 // database connection parameters (from railway)
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_USERNAME', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_NAME', getenv('DB_NAME'));
+define('DB_HOST', getenv('DB_HOST') ?: 'mysql.railway.internal');
+define('DB_USERNAME', getenv('DB_USER') ?: 'root');
+define('DB_PASSWORD', getenv('DB_PASSWORD') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'railway');
 define('DB_PORT', getenv('DB_PORT') ?: 3306); // default MySQL = 3306
 
 // connexion to the database
