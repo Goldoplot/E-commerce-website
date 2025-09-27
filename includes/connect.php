eedfbd268159445edbefb8337bbd6adc73a9db12
tuan nelet <tuannelet@gmail.com>
2025-09-27 14:31:24 +0700

    creating docker file

--- includes/connect.php
+++ includes/connect.php
@@ -1,24 +1,21 @@
 <?php
-// Database connection parameters (from Railway)
+
+// database connection parameters (from railway)
 define('DB_HOST', getenv('DB_HOST') ?: 'mysql.railway.internal');
 define('DB_USERNAME', getenv('DB_USER') ?: 'root');
 define('DB_PASSWORD', getenv('DB_PASSWORD') ?: '');
 define('DB_NAME', getenv('DB_NAME') ?: 'railway');
 define('DB_PORT', getenv('DB_PORT') ?: 3306); // default MySQL = 3306
 
-try {
-    // building PDO
-    $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8";
+// connexion to the database
+$con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
 
-    // connecting PDO
-    $con = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
-
-    // managing errors
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // if it works well
-} catch (PDOException $e) {
-    // if it doesn't work well
-    die("Erreur connexion PDO: " . $e->getMessage());
+// Check connection
+if (!$con) {
+    die("Database connection failed: " . mysqli_connect_error());
 }
+
+// Set the character set to utf8
+mysqli_set_charset($con, "utf8");
+
 ?>
